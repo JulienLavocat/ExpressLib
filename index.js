d65b57a106923c30c6b8c18a807ea9e3c6908b90
Aayyi <julien.lavocat@gmail.com>
2019-06-29 12:17:02 +0200

    Refactored errors

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ module.exports = {
     redis: require("./lib/redis").redis,
     mongodb: require("./lib/mongo").mongo,
     middlewares: require("./middlewares"),
-    HttpError: require("./lib/HttpError"),
+    HttpError: require("./lib/errors/HttpError"),
     jwt: require("./lib/jwt")
 };
--- middlewares/apiKey.js
+++ middlewares/apiKey.js
@@ -1,4 +1,4 @@
-const HttpError = require("../lib/HttpError");
+const HttpError = require("../lib/errors/HttpError");
 const redis = require("../lib/redis");
 
 module.exports = async function (req, res, next) {
